90869fb1e62ee3b88c18556ba042b0f51c487cc1
Dhanush Gopinath <dhanush@geektrust.in>
2023-08-30 16:31:20 +0530

    new starter kit for java interview process

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+    public static void main(String[] args)  {
+        for (int i = 0; i < args.length; i++) {
+            String input = args[i];
+            //Write your logic here to process input commands.
+            String output = ""; //process the input command and get the output
+            //Once it is processed print the output using the command System.out.println()
+            System.out.println(output);
+        }
+	}
+}
