60bc82ed27eed5ad92c1705408ade5701d9a82eb
Aneesh Anil <aneesh@geektrust.in>
2023-09-22 13:25:03 +0530

    added Apache Ant build capabilities.

---
+++ src/main/java/Main.java
@@ -0,0 +1,20 @@
+public class Main{
+
+    /*
+     ***********************************************
+     * This is the driver code. Don't change it!!!
+     * *********************************************
+     */
+    public static void main(String[] args){
+        for (String cmd : args) {
+            handle(cmd);
+        }
+    }
+
+    /*
+     * Edit this to start handling the commands.
+     */
+    private static void handle(String cmdLine) {
+        System.out.println(cmdLine);
+    }
+}
---
+++ src/test/java/SampleTests.java
@@ -0,0 +1,16 @@
+/*
+ * A sample test class to help you get started.
+ * Can be deleted or renamed as appropriate.
+ * Make sure these classes end with `Tests`
+ */
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class SampleTests {
+    @Test
+    void testSomething() {
+        assertEquals(2, 1 + 1);
+    }
+}
