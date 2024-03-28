955d1800e97d2c667fce1c09b03857200c482f6c
Joydeep Bhattacharya <58521295+linkwithjoydeep@users.noreply.github.com>
2024-03-28 14:51:32 +0530

    added comments for input structure

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,8 @@ public class Main{
      * *********************************************
      */
     public static void main(String[] args){
+        // Format of the 'args' array: <Number of Travelers> <Destination> <Trip Type>
+        // Example: 3 Paris round
         for (String cmd : args) {
             //handle each command passed in and implement the solution.
         }
