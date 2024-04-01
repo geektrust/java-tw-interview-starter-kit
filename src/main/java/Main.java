4d5100641bc63dcf07af634301eca7704474d363
sebin <sebin@geektrust.in>
2024-04-01 11:24:41 +0530

    No need to loop through args as there are not commands in the input.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,10 +7,7 @@ public class Main{
      */
     public static void main(String[] args){
         // Format of the 'args' array: <Number of Travelers> <Destination> <Trip Type>
-        // Example: 3 Paris round
-        for (String cmd : args) {
-            //handle each command passed in and implement the solution.
-        }
+        // Example: [3, Paris, round]
     }
 
 
