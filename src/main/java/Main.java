fe02f5acbf74dc66eb687e52cf63d35d078ec5ff
INDRA KUMAR R <indra@geektrust.in>
2025-04-21 17:09:48 +0530

    feat: generalize starter-kit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,13 +8,12 @@ public class Main{
     public static void main(String[] args){
         /*
          * Format of the 'args' array: `<Number of Travelers> <Destination> <Trip Type>`
-         *  Example: ["3 Paris round"]
+         * Example: ["3 Paris round"]
          * 
          * The code evaluator will execute this code by using the command 
          * java -jar dist/lib/dist.jar '3 Paris one-way'
          * 
          * So the value of the variable "input" given below will be the string "3 Paris one-way".
-         * 
          */
         
         handle(args[0]);
@@ -23,16 +22,16 @@ public class Main{
     /*
      * This method parses each input and assigns it into different variables.
      * 
-     * The value of the function parameter "input" will be of the format - "3 Paris one-way". 
+     * The value of the function parameter "input" could be of this format - "3 Paris one-way".
      * We split the string and retrieve the input data appropriately. 
      * Once retrieved, it is available as String data for you to implement the solution.
      *  
      */
     private static void handle(String input) {
         String[] inputList = input.trim().split(" ");
-        String noOfTravelers = inputList[0];
-        String destination = inputList[1];
-        String tripType = inputList[2];
+        String input1 = inputList[0];
+        String input2 = inputList[1];
+        String input3 = inputList[2];
 
         /*
          * Implementation of the solution should begin after this. Please make sure you convert the variables into appropriate data types required for the solutioning. 
